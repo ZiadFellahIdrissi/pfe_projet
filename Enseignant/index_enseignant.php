a0621f65cd4a11b07944c9ed98e5ccedd02f1188
ziadfellahidrissi <ziad.fe1999@gmail.com>
2020-04-05 20:38:26 +0100

    add teacher page

---
+++ Enseignant/index_enseignant.php
@@ -0,0 +1,55 @@
+<?php
+include '../connectionDB.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="../layout/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+    <title>test</title>
+    <!-- rahe deja telechargiite bootstarp 5aliw dakchi f blasto ou rahe i5dame likom -->
+</head>
+
+<body>
+    <!--hadi container jam3a kooooooooolckii-->
+    <div class="container mt-3 mb-3" style="width:100%;">
+
+        <!-- bloc de menu -->
+        <ul class="nav nav-tabs" id="myTab" role="tablist">
+            <li class="nav-item">
+                <a class="nav-link " href="../index_filiere.php" aria-selected="false"><b>Filiere</b></a>
+            </li>
+            <li class="nav-item ">
+                <a class="nav-link active" href="./index_enseignant.php" aria-selected="true"><b>Etudiant</b></a>
+            </li>
+            <li class="nav-item ">
+                <a class="nav-link " href="../index_etudiant.php" aria-selected="false"><b>Etudiant</b></a>
+            </li>
+        </ul>
+        <!-- end bloc de menu -->
+
+
+        <div class="tab-content" id="myTabContent">
+            <div class="tab-pane fade show active" role="tabpanel" aria-labelledby="home-tab">
+                <div class="container mt-3 mb-3">
+                    kjdbfkdbfkbsdkfbsdkjfdksjbdskhbkfbdkfbkdbdhkbkdbckdhbdkhbckdkhbkvd
+                    <br>kfbdkfbkdbdhkbkdbckdhbdkfbdkfbkdbdhkbkdbckdhbdkhbckkhbck<br>
+                    kjdbfkdbfkbsdkfbsdkjfdksjbdskkfbdkfbkdbdhkbkdbckdhbdkhbckhbdkhbkvd
+                    <br>kfbdkfbkdbdhkbkdbckdhbdkkfbdkfbkdbdhkbkdbckdhbdkhbckhbck<br>
+                    kjdbfkdbfkbsdkfbsdkjfdksjbdkfbdkfbkdbdhkbkdbckdhbdkhbckskhbdkhbkvd
+                    <br>kfbdkfbkdbdhkbkdbckdhkfbdkfbkdbdhkbkdbckdhbdkhbckbdkhbck<br>
+                    kjdbfkdbfkbsdkfbsdkjfdksjbkfbdkfbkdbdhkbkdbckdhbdkhbckdskhbdkhbkvd
+                    <br>kfbdkfbkdbdhkbkdbckdhbdkhbck<br>
+
+                </div>
+            </div>
+        </div>
+
+        <script type="text/javascript" src="../layout/js/jquery-3.4.1.min.js"></script>
+        <script type="text/javascript" src="../layout/js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
