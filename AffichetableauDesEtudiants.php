a6bcd4aa04c65acc491d738eab567826e0441ed3
ziadfellahidrissi <ziad.fe1999@gmail.com>
2020-04-05 02:35:36 +0100

    add file

--- AffichetableauDesEtudiants.php
+++ AffichetableauDesEtudiants.php
@@ -36,7 +36,6 @@ if ($resultatcheck > 0) {
                     </a>
                 </td>
                 <td>
-                    <!-- <img id="<?php echo $row["code_apoge"] ?>" class="Open_modifierUnEtudiant" data-toggle="modal" width=20 heigth=20 src="https://bit.ly/2UwQb08"> -->
                     <input type="button" value="Modifier" id="<?php echo $row["code_apoge"] ?>" data-toggle="modal" class="btn btn-info btn-xs Open_modifierUnEtudiant">
                 </td>
             </tr>
