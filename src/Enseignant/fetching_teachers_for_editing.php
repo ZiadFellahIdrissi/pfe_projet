aaac8a21bb927b20d54cf42815a5f2d660d0e4c2
clevernessisamyth <63174192+clevernessisamyth@users.noreply.github.com>
2020-04-27 00:54:56 +0000

    added modules

--- src/Enseignant/fetching_teachers_for_editing.php
+++ src/Enseignant/fetching_teachers_for_editing.php
@@ -4,10 +4,7 @@
         $id=$_GET["code"];
         $sql="SELECT * FROM enseignant 
                 WHERE id_enseignant=$id";
-
         $resultat1=mysqli_query($conn,$sql);
         $Myrow = mysqli_fetch_array($resultat1);
         echo json_encode($Myrow);
-    
-       
 ?>
